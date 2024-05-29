d145389e9c0afbc729313cbe453d04b66228a573
Gabriel Tomiazzi <gabrieltomiazzi@outlook.com>
2024-05-28 21:09:40 -0300

    testing paymentScreen

--- App.tsx
+++ App.tsx
@@ -1,19 +1,23 @@
 import { useFonts } from 'expo-font';
-import Camera from './src/pages/Camera';
 import UserProfile from './src/pages/UserProfile';
 import { Poppins_400Regular, Poppins_700Bold } from '@expo-google-fonts/poppins';
 import { ThemeProvider } from 'styled-components/native';
 import theme from './src/global/theme';
+import { StripeProvider } from '@stripe/stripe-react-native';
+import PaymentScreen from './src/pages/Payment';
 
 export default function App() {
+  const stripeKey = 'pk_test_51PIb3w046m3vjyit5zcqiV3NuE2JLJItfvCZpi6V63LpVUyW1S6JRwpGhTmrAlFf7HH2ydCQeIJ0gRPJN96Bkfuu00BibGcQuK';
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
     Poppins_700Bold
   });
 
   return (
-    <ThemeProvider theme={theme}>
-      <UserProfile />
-    </ThemeProvider>
+    <StripeProvider publishableKey={stripeKey}>
+      <ThemeProvider theme={theme}>
+        <PaymentScreen />
+      </ThemeProvider>
+    </StripeProvider>
   );
 }
--- src/components/CircleContainer/style.ts
+++ src/components/CircleContainer/style.ts
@@ -1,12 +1,13 @@
-import { Image } from 'react-native';
+import { Image, TouchableOpacity } from 'react-native';
 import styled from 'styled-components/native';
 
 interface CircleProps {
   size: number;
   color: string;
+  onPress?: () => void;
 }
 
-export const CircleContainer = styled.View<CircleProps>`
+export const CircleContainer = styled(TouchableOpacity)<CircleProps>`
   width: ${(props) => props.size}px;
   height: ${(props) => props.size}px;
   border-radius: ${(props) => props.size / 2}px;
