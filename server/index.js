e34cb2cab4c0a4296ee7055125603156c2f6f7f3
Gabriel Tomiazzi <gabriel.tomiazzi@moinho.regio>
2024-06-14 16:31:29 -0300

    fix: correcao API local

--- server/index.js
+++ server/index.js
@@ -1,10 +1,31 @@
-import express from 'express';
+const express = require('express');
+const cors = require('cors');
+const stripe = require('stripe')('sk_test_51PIb3w046m3vjyitvfpzjggMAEVSzJq8mOoIZYSumJpgqOxllJ760TVuneCKWFuz0cikr28ZJg0rhdSWqd0K7dDj00CjV8zztD');
 
 const app = express();
-const port = 3000;
-const PUBLISHABLE_KEY = 'pk_test_51PIb3w046m3vjyit5zcqiV3NuE2JLJItfvCZpi6V63LpVUyW1S6JRwpGhTmrAlFf7HH2ydCQeIJ0gRPJN96Bkfuu00BibGcQuK';
-const SECRET_KEY = 'sk_test_51PIb3w046m3vjyitvfpzjggMAEVSzJq8mOoIZYSumJpgqOxllJ760TVuneCKWFuz0cikr28ZJg0rhdSWqd0K7dDj00CjV8zztD';
 
-app.listen(port, () => {
-    console.log(`Server running on por ${port}`)
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cors());
+
+app.post('/payment-test', async (_req, res) => {
+  const customer = await stripe.customers.create();
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 1000,
+    currency: 'brl',
+    customer: customer.id,
+    automatic_payment_methods: {
+      enabled: true,
+    }
+  });
+
+  res.json({
+    paymentIntent: paymentIntent.client_secret,
+    customer: customer.id,
+    publishableKey: 'pk_test_51PIb3w046m3vjyit5zcqiV3NuE2JLJItfvCZpi6V63LpVUyW1S6JRwpGhTmrAlFf7HH2ydCQeIJ0gRPJN96Bkfuu00BibGcQuK'
+  });
+})
+
+app.listen(3000, () => {
+  console.log('Running on port ' + 3000)
 })
