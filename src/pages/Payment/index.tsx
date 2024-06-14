81006117423201d9c29951ae8bd56f1a71f57c32
Gabriel Tomiazzi <gabriel.tomiazzi@moinho.regio>
2024-06-14 16:32:34 -0300

    fix: correcao Network Error

--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -1,29 +1,56 @@
-import { CardField, useStripe } from '@stripe/stripe-react-native';
+import { StripeProvider, useStripe } from '@stripe/stripe-react-native';
+import { useState } from 'react';
+import { Button, Platform, SafeAreaView, Text } from 'react-native';
 
-export default function PaymentScreen() {
-  const { confirmPayment } = useStripe();
+export default function Payment() {
+  const stripeKey = 'pk_test_51PIb3w046m3vjyit5zcqiV3NuE2JLJItfvCZpi6V63LpVUyW1S6JRwpGhTmrAlFf7HH2ydCQeIJ0gRPJN96Bkfuu00BibGcQuK';
+  const { initPaymentSheet, presentPaymentSheet } = useStripe();
+  const [ loading, setLoading ] = useState(false);
+
+  const API_URL = Platform.OS == 'ios' ? "http://localhost:3000" : "http://127.0.0.1:3000";
+
+  const fetchPaymentSheetParams = async () => {
+    const res = await fetch(`${API_URL}/payment-test`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const { paymentIntent, customer } = await res.json();
+
+    return {
+      paymentIntent, 
+      customer
+    }
+  }
+
+  const initializePaymentSheet = async () => {
+    const { paymentIntent, customer } = await fetchPaymentSheetParams();
+
+    const { error } = await initPaymentSheet({
+      merchantDisplayName: 'Xanaia',
+      customerId: customer,
+      paymentIntentClientSecret: paymentIntent,
+      defaultBillingDetails: {
+        name: 'Hanana'
+      }
+    });
+    if (!error) {
+      setLoading(true);
+    }
+  }
 
   return (
-    <CardField
-      postalCodeEnabled={true}
-      placeholders={{
-        number: '4242 4242 4242 4242',
-      }}
-      cardStyle={{
-        backgroundColor: '#FFFFFF',
-        textColor: '#000000',
-      }}
-      style={{
-        width: '100%',
-        height: 50,
-        marginVertical: 30,
-      }}
-      onCardChange={(cardDetails) => {
-        console.log('cardDetails', cardDetails);
-      }}
-      onFocus={(focusedField) => {
-        console.log('focusField', focusedField);
-      }}
-    />
+    <StripeProvider
+      publishableKey={stripeKey}
+    >
+      <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Hanana</Text>
+        <Button 
+          title='Teste'
+          onPress={initializePaymentSheet}
+        />
+      </SafeAreaView>
+      </StripeProvider>
   );
 }
